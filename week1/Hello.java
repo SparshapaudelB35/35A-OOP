5b5324bb0c742f00b94f1e12b6197b0c2cc4acdf
sparsha <sparsha@gmail.com>
2024-04-11 10:19:04 +0545

    week 1

---
+++ week1/Hello.java
@@ -0,0 +1,9 @@
+package week1;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.print("This is my first progrma\nI am on module ST4003CEM");
+    }
+    
+}
