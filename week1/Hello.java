7dd20241a51908065062f92d03f2e75b33804edd
sparsha <sparsha@gmail.com>
2024-04-15 06:54:54 +0545

    weekly task completed

--- week1/Hello.java
+++ week1/Hello.java
@@ -2,8 +2,23 @@ package week1;
 
 public class Hello {
     public static void main(String[] args) {
+        /*1. Write a program that prints Hello World! to the console. Save the program as Hello.java.
+        System.out.print (not println) and produces the same output. */
         System.out.println("Hello World!");
-        System.out.print("This is my first progrma\nI am on module ST4003CEM");
+        /*2. Extend/Modify the above program to print two additional lines:
+        This is my first program
+        I am on module ST4003CEM*/
+        System.out.print("This is my first program\nI am on module ST4003CEM\n");
+        /*3. Write a program that prints the following output:
+            A "quoted" String is
+            'much' better if you learn
+            the rules of "escape sequences."
+            Also, "" represents an empty
+            String. Don't forget: use \"
+            instead of " !
+            '' is not the same as " */
+        System.out.print("A \"quoted\" string is\n\'much\' better if  you learn\nthe rules of\"escape sequences.\n\"\" represents an empty in Java\nString. Don't forget:use\\\"\ninstead of\"!\n\"is not the same as\"");
+
     }
     
 }
