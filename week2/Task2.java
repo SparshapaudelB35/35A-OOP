368d6c5b2070f81df07781856826a5683986b87c
sparsha <sparsha@gmail.com>
2024-04-22 07:27:44 +0545

    week2 task2 completed

---
+++ week2/Task2.java
@@ -0,0 +1,13 @@
+package week2;
+
+public class Task2 {
+    public static void main(String[] args) {
+        /*task 2
+         * write a program to print simple intrest from variable
+         */
+        int principle=100;
+        Double time=2.5;
+        int rate=13;
+        System.out.println("the simple intrest is "+(principle*time*rate)/100);
+    }
+}
