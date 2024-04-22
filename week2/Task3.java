7f474e00a70a7b41045d693bd78cbd5dad09e3e7
sparsha <sparsha@gmail.com>
2024-04-22 07:28:03 +0545

    week2 task3 completed

---
+++ week2/Task3.java
@@ -0,0 +1,13 @@
+package week2;
+
+public class Task3 {
+    public static void main(String[] args) {
+        /*task 3
+         * write a program to print the area nad perimeter of rectangle
+         */
+        int length=10;
+        int breadth=20;
+        System.out.println("the area of rectangle is "+(length*breadth));
+        System.out.println("the perimeter of rectangle is "+2*(length+breadth));
+    }
+}
