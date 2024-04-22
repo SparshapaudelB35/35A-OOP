92d8ff17e9b328a65b21e1225b442dbbed1bc66a
sparsha <sparsha@gmail.com>
2024-04-22 07:28:20 +0545

    Week2 task4 completed

---
+++ week2/Task4.java
@@ -0,0 +1,12 @@
+package week2;
+
+public class Task4 {
+    public static void main(String[] args) {
+        //task 4 TAsk1 with ternary operator
+        int b=10;
+        String check=b>=18?"True":"false";
+        System.out.println(check);
+
+
+    }
+}
