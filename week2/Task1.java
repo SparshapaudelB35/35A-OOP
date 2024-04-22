bfdb0631701d89a647008d8954a04d7eb13a423c
sparsha <sparsha@gmail.com>
2024-04-22 07:27:14 +0545

    week2 task1 completed

---
+++ week2/Task1.java
@@ -0,0 +1,11 @@
+package week2;
+
+public class Task1{
+    public static void main(String[] args) {
+        /*task1
+        * write a program to print wheather a variable is greater or equal to 18 
+        */
+        int a=10;
+        System.out.println(a>=18);
+    }
+}
