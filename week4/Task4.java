eb8baf8f90b3711496a893d1b3025cd01d312d8d
sparsha <sparsha@gmail.com>
2024-05-03 14:38:58 +0545

    Week4 task4 completed

---
+++ week4/Task4.java
@@ -0,0 +1,18 @@
+package week4;
+
+public class Task4 {
+    public static void main(String[] args) {
+        /*
+         * 4. Write a program that reads the width and generates a corresponding square
+         * of *. For example, if width = 5, output is:
+         *****
+         *****
+         *****
+         *****
+         ***** 
+         */
+        for(int i=0;i<5;i++){
+            System.out.println("*****");
+        }
+    }
+}
